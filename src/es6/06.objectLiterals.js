38dfc69c935d6d922588047eaa50a2b9769ce02b
Andrés Zapata <andresmauricio030400@gmail.com>
2023-03-03 13:56:35 -0500

    Implementación en ES6

---
+++ src/es6/06.objectLiterals.js
@@ -0,0 +1,10 @@
+// ENAHCED OBJECT LITERALS.
+function newUser(user, age, country, uId) {
+  return {
+    user,
+    age,
+    country,
+    uId,
+  };
+}
+console.log(newUser("BlackFenix", 23, "Colombia", 1));
