a1b8c8bf5e9a0aab94afa2b033835b8de5a9711e
Andrés Zapata <andresmauricio030400@gmail.com>
2023-03-03 12:32:31 -0500

    Implementación en ES6

---
+++ src/es6/03.strings.js
@@ -0,0 +1,16 @@
+let hello = "Hello";
+let world = "World";
+let epicPhrase = hello + " " + world + "!";
+console.log(epicPhrase);
+
+// TEMPLATE LITERALS.
+let epicPhrase2 = `${hello} ${world}!`;
+console.log(epicPhrase2);
+
+// MULTI-LINES.
+let string = "Esto es una línea. \n " + "Esto es otra línea.";
+console.log(string);
+
+let string2 = `Esto es una línea.
+ Esto es otra línea.`;
+console.log(string2);
