85fd82c79c0c14ac2127a7b8d30ab90d170caf6b
Andrés Zapata <andresmauricio030400@gmail.com>
2023-03-07 11:35:57 -0500

    Implementación en ES12

---
+++ src/es12/02.replaceAll.js
@@ -0,0 +1,5 @@
+const string = "JavaScript es un maravilloso lenguaje de programación.";
+console.log(string);
+
+const replacedString = string.replaceAll("JavaScript", "TypeScript");
+console.log(replacedString);
