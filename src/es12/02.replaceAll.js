be9e708b4d2ee5470477c73aceebeae48d57ee5f
Andrés Zapata <116312529+BlackFenix07@users.noreply.github.com>
2023-03-07 11:00:45 -0500

    Delete 02.replaceAll.js

--- src/es12/02.replaceAll.js
+++
@@ -1,5 +0,0 @@
-const string = "JavaScript es un maravilloso lenguaje de programación.";
-console.log(string);
-
-const replacedString = string.replaceAll("JavaScript", "TypeScript");
-console.log(replacedString);
